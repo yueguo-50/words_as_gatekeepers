64458bab7aa0314ee05867e074000b5cb933e35e
yueguo-50 <yueguo50@gmail.com>
2023-09-01 01:39:31 +0000

    modified:   run_gantry.sh

--- run_gantry.sh
+++ run_gantry.sh
@@ -2,4 +2,4 @@ gantry run \
     --beaker-image 'yueg/yue_0831' \
     --cluster 'ai2/s2-cirrascale' \
     --venv 'base' \
-    --python ./code/type_jargon/word_counts_per_fos.py
+    -- python './code/type_jargon/word_counts_per_fos.py'
